8fd60abe49c14146cd950df370b5008d6e9a517c
Vinicius <vm.murad@gmail.com>
2020-02-08 19:13:29 -0300

    add option to update profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-    return <div />;
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Full name" />
+                <Input name="email" type="email" placeholder="Email" />
+                <hr />
+
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Current password"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="New password"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm password"
+                />
+
+                <button type="submit">Update profile</button>
+            </Form>
+
+            <button type="button">Logout</button>
+        </Container>
+    );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+    max-width: 600px;
+    margin: 50px auto;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        margin-top: 30px;
+
+        input {
+            background: rgba(0, 0, 0, 0.1);
+            border: 0;
+            border-radius: 4px;
+            height: 44px;
+            padding: 0 15px;
+            color: #fff;
+            margin: 0 0 10px;
+
+            &::placeholder {
+                color: rgba(255, 255, 255, 0.7);
+            }
+        }
+    }
+
+    span {
+        color: #fff;
+        align-self: center;
+        margin: 0 0 10px;
+    }
+
+    hr {
+        border: 0;
+        height: 1px;
+        background: rgba(255, 255, 255, 0.1);
+        margin: 10px 0 20px;
+    }
+
+    button {
+        margin: 5px 0 0;
+        height: 44px;
+        background: #3b9eff;
+        font-weight: bold;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        color: #fff;
+        transition: bacgrkound 0.2s;
+
+        &:hover {
+            background: ${darken(0.03, '#3b9eff')};
+        }
+    }
+
+    > button {
+        width: 100%;
+        margin: 10px 0 0;
+        height: 44px;
+        background: #f64c75;
+        font-weight: bold;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        color: #fff;
+        transition: bacgrkound 0.2s;
+
+        &:hover {
+            background: ${darken(0.08, '#f64c75')};
+        }
+    }
+`;
